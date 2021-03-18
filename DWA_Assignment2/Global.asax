8b79f434239028d2dc2c535f4ad251a05ca5dd4e
Jordan King <jordan_king_77@hotmail.co.uk>
2021-03-18 16:00:23 +0000

    Add project files.

---
+++ DWA_Assignment2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DWA_Assignment2.WebApiApplication" Language="C#" %>
---
+++ DWA_Assignment2/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DWA_Assignment2.Startup))]
+
+namespace DWA_Assignment2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
